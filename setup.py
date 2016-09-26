6ab682fa92dbbcf8c4029decd013c75c3445dddf
Ryan Fredlund <rfredlund13@gmail.com>
2016-09-25 22:29:17 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "vixenconverter",
+    version = "0.0.1",
+    author = "Tesla Works",
+    description = ("Converts *.vix to Proton format"),
+    license = "MIT",
+    url = "https://github.com/teslaworksumn/proton-vixen-converter",
+    packages=['vixenconverter', 'tests']
+)
