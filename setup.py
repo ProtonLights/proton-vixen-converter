a655dc7e788ff4d957f11c8a33546bbdf904d3cb
Ryan Fredlund <bookdude13@users.noreply.github.com>
2016-10-03 19:07:43 -0500

    Added name to authors (#2)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name = "vixenconverter",
     version = "0.0.1",
-    author = "Tesla Works",
+    author = "Ryan Fredlund <fredl027>, ",
     description = ("Converts *.vix to Proton format"),
     license = "MIT",
     url = "https://github.com/teslaworksumn/proton-vixen-converter",
