0a78df977ccc8fce8ab7bed011d014a307b92023
Kayla Engelstad <engel813@umn.edu>
2016-10-07 08:12:05 -0700

    Added name to authors list (#5)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name = "vixenconverter",
     version = "0.0.1",
-    author = "Ryan Fredlund <fredl027>, ",
+    author = "Ryan Fredlund <fredl027>, ", "Kayla Engelstad <engel813>,",
     description = ("Converts *.vix to Proton format"),
     license = "MIT",
     url = "https://github.com/teslaworksumn/proton-vixen-converter",
